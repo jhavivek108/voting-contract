caed0699f6173742ad24c5401b401bae6aee4757
jhavivek108 <jhavivek2404@gmail.com>
2026-01-19 16:20:07 +0000

    feat: implement candidate and voter registration and voting logic

--- programs/vote/src/lib.rs
+++ programs/vote/src/lib.rs
@@ -8,71 +8,110 @@ declare_id!("3ZynTBMwRYRhi8jtyGZsJYappPo4Qa7WRwZompCa1tS3");
 pub mod vote {
     use super::*;
 
-    pub fn register_candidate(ctx: Context<RegisterCandidate>) -> Result<()> {  
+    pub fn register_candidate(ctx: Context<RegisterCandidate>, name: String) -> Result<()> {
+        let candidate: &mut _= &mut ctx.accounts.candidate;
+        candidate.set_inner(Candidate {
+            c_name: name,
+            vote_count: 0,
+            candidate_id: ctx.accounts.owner.key(),
+        });
+
         Ok(())
     }
 
-    pub fn register_voter(ctx: Context<RegisterVoter>) -> Result<()> {  
+    pub fn register_voter(ctx: Context<RegisterVoter>, name: String ) -> Result<()> { 
+        let voter: &mut _= &mut ctx.accounts.voter;
+        voter.set_inner(Voter {
+            v_name: name,
+            is_voted: false,
+            voter_id: ctx.accounts.owner.key(),
+        });
+         
         Ok(())
     }
-}
 
-#[account]
-#[derive(InitSpace)]
-pub struct Candidate {
-    #[max_len(20)]
-    c_name: String,
-    vote_count: u64,
-    candidate_id: Pubkey,
-}
+    pub fn cast_vote(ctx: Context<CastVote>) -> Result<()> { 
+        let voter: &mut _= &mut ctx.accounts.voter;
+        let candidate: &mut _= &mut ctx.accounts.candidate;
 
-#[account]
-#[derive(InitSpace)]
-pub struct Voter {
-    #[max_len(20)]
-    v_name: String,
-    is_voted: bool,
-    voter_id: Pubkey,
-}
+        require!(ctx.accounts.voter_signer.key() == voter.voter_id, VoteError::UnauthorizedVoter);
+        require!(voter.is_voted== false, VoteError::AlreadyVoted);
+        
+        cnadidate.vote_count += 1;
+        voter.is_voted = true;
+        Ok(())
+    }
 
 
-#[derive(Accounts)]
-#[instruction(c_name: String)]
-pub struct RegisterCandidate<'info> {
-    #[account(mut)]
-    pub owner: Signer<'info>,
-    #[account(init,
-    payer= owner,
-    space= 8 + Candidate::INIT_SPACE,
-    seeds= [c_name.as_bytes(), owner.key().as_ref()],
-    bump)]
-    pub candidate: Account<'info, Candidate>,
-    pub system_program: Program<'info, System>,
-}
 
-#[derive(Accounts)]
-#[instruction(v_name: String)]
-pub struct RegisterVoter<'info> {
-    #[account(mut)]
-    pub owner: Signer<'info>,
-    #[account(init,
-    payer= owner,
-    space= 8 + Voter::INIT_SPACE,
-    seeds= [v_name.as_bytes(), owner.key().as_ref()],
-    bump)]
-    pub voter: Account<'info, Voter>,
-    pub system_program: Program<'info, System>,
-}
 
-#[derive(Accounts)]
+    #[account]
+    #[derive(InitSpace)]
+    pub struct Candidate {
+        #[max_len(20)]
+        c_name: String,
+        vote_count: u64,
+        candidate_id: Pubkey,
+    }
 
-pub struct CastVote<'info> {
-    #[account(mut)]
-    pub voter: Account<'info, Voter>,
+    #[account]
+    #[derive(InitSpace)]
+    pub struct Voter {
+        #[max_len(20)]
+        v_name: String,
+        is_voted: bool,
+        voter_id: Pubkey,
+    }
 
-     #[account(mut)]
-    pub candidate: Account<'info, Candidate>,
 
-    #[account(mut)]
-    pub voter_signer: Signer<'info>
+    #[derive(Accounts)]
+    #[instruction(c_name: String)]
+    pub struct RegisterCandidate<'info> {
+        #[account(mut)]
+        pub owner: Signer<'info>,
+        #[account(init,
+        payer= owner,
+        space= 8 + Candidate::INIT_SPACE,
+        seeds= [c_name.as_bytes(), owner.key().as_ref()],
+        bump)]
+        pub candidate: Account<'info, Candidate>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[derive(Accounts)]
+    #[instruction(v_name: String)]
+    pub struct RegisterVoter<'info> {
+        #[account(mut)]
+        pub owner: Signer<'info>,
+        #[account(init,
+        payer= owner,
+        space= 8 + Voter::INIT_SPACE,
+        seeds= [v_name.as_bytes(), owner.key().as_ref()],
+        bump)]
+        pub voter: Account<'info, Voter>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[derive(Accounts)]
+
+    pub struct CastVote<'info> {
+        #[account(mut)]
+        pub voter: Account<'info, Voter>,
+
+        #[account(mut)]
+        pub candidate: Account<'info, Candidate>,
+
+        #[account(mut)]
+        pub voter_signer: Signer<'info>,
+    }
+
+    #[error_code]
+
+    pub enum VoterError {
+        #[msg("You are not authorized to vote.")]
+        UnauthorizedVoter,
+        #[msg("You have already voted.")]
+        AlreadyVoted,
+    }
+
 }
